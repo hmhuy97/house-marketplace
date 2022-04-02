eb783b06537f9ceebe4dd8cea386a4a84ccb7832
hmhuy97 <39719651+hmhuy97@users.noreply.github.com>
2022-04-02 18:32:49 +0700

    4 User Sign In

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { getAuth } from "firebase/auth";
 
 function Profile() {
-  return <h1>Profile</h1>;
+  const [user, setUser] = useState(null);
+
+  const auth = getAuth();
+  useEffect(() => {
+    setUser(auth.currentUser);
+    console.log(auth.currentUser);
+  }, []);
+  return user ? (
+    <h1>{user.displayName ? user.displayName : "Không có tên"}</h1>
+  ) : (
+    "Not Logged In"
+  );
 }
 
 export default Profile;
