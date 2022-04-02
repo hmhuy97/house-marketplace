9fa9cf4c103c874a97be823455070459d24948a9
hmhuy97 <39719651+hmhuy97@users.noreply.github.com>
2022-04-02 22:51:56 +0700

    7. 6p53

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { getAuth } from "firebase/auth";
-
+import { getAuth, updateProfile } from "firebase/auth";
+import { updateDoc } from "firebase/firestore";
+import db from "../firebase.config";
 function Profile() {
   const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
   const [user, setUser] = useState(null);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
+
+  const { name, email } = formData;
 
   const navigate = useNavigate();
 
@@ -16,6 +20,17 @@ function Profile() {
     auth.signOut();
     navigate("/");
   };
+
+  const onSubmit = () => {
+    console.log(123);
+  };
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -26,6 +41,33 @@ function Profile() {
           </button>
         </p>
       </header>
+
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? "done" : "change"}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form action="">
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? "profileName" : "profileNameActive"}
+              disabled={!changeDetails}
+              value={name ?? "Không có tên"}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
