fd37a2fdb91e41670c5f952a034f0121ee268f24
hmhuy97 <39719651+hmhuy97@users.noreply.github.com>
2022-04-06 21:09:48 +0700

    10. Google OAuth - sign in w google f14

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,6 +10,7 @@ import {
 import { toast } from "react-toastify";
 import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
+import OAuth from "../components/OAuth";
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
@@ -104,6 +105,7 @@ function SignUp() {
           </div>
         </form>
         {/* GG OAuth */}
+        <OAuth />
         <Link to="/sign-in" className="registerLink">
           Sign In Instead
         </Link>
